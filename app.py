e0e316ec7856c38022d56fbc2c35f05d2bb1285b
Mukul Kumar <101646861+MukulKumar56@users.noreply.github.com>
2024-10-30 15:37:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -54,5 +54,5 @@ def convert_pdf_to_word():
 
     return response
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
